55644e0394bfbe8d61a936ba2a2fbbcd16e4140b
adriano-65 <121606279+adriano-65@users.noreply.github.com>
2022-12-29 16:40:39 +0300

    intern setup II (#2)

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,14 +1,11 @@
-from .base import *
+from .base import *  # noqa
+from .base import List, env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.get("SECRET_KEY", default="unsafe-secret-key")
+SECRET_KEY = env.str("SECRET_KEY", default="unsafe-secret-key")
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-
-ALLOWED_HOSTS: List[str] = []
+ALLOWED_HOSTS: List[str] = ["*"]
